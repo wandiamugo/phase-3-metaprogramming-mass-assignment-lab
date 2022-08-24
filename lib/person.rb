396a515547fd4335dd74280e3c41ceef519923ac
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-08-24 17:00:07 +0300

    Done'
    clear

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
+
 class Person
-  # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh
+  attr_accessor :weight, :handed, :complexion, :t_shirt_size
+  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+dennis_attributes = { name: "Ruyo", hair_color: "Black" }
+dennis = Person.new(dennis_attributes)
+p dennis.name
+p dennis.hair_color
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+p susan.name      
+p susan.height    
+p susan.eye_color
